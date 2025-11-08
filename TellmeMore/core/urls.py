d03eb95c4ecb62df77b291069842c41ed61563ca
priyanshi <priyanshikhandelwal75@gmail.com>
2025-11-08 22:35:36 +0530

    views and urls added

--- TellmeMore/core/urls.py
+++ TellmeMore/core/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
 
     path("",views.home_view , name="home"),
-    
+    path("about/",views.about_view , name="about"),
+    path("how_to/",views.how_to_view , name="how_to"),
+
 ]
--- TellmeMore/core/views.py
+++ TellmeMore/core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render , redirect
 # Create your views here.
 def home_view(request):
     return render(request, "home.html")
+
+def about_view(request):
+    return render(request, "about.html")
+
+def how_to_view(request):
+    return render(request, "how_to.html")
