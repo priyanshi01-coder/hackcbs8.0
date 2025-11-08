3396207797f23e0f32bd7ee836b7bd8728663c43
priyanshi <priyanshikhandelwal75@gmail.com>
2025-11-09 04:05:23 +0530

    urls and views

--- TellmeMore/dashboard/views.py
+++ TellmeMore/dashboard/views.py
@@ -6,3 +6,16 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def dashboard_view(request):
     return render(request, "dashboard/dashboard.html")
+
+@login_required
+def my_sessions(request):
+    return render(request, "dashboard/my_sessions.html")
+
+@login_required
+def uploaded_items(request):
+    return render(request, "dashboard/uploaded_items.html")
+
+@login_required
+def analytics(request):
+    from dashboard.models import InterviewSession, SessionAnalytics
+    from django.db.models import Avg, Count, Max
